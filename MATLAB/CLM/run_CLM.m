dcb3c28a6f0d4b80bd8463bf6d497557e4e8605f
michaelios <dan112@madaimas.org.il>
2024-11-21 01:11:11 +0200

    mitkarev lesimulatzia

--- MATLAB/CLM/run_CLM.m
+++ MATLAB/CLM/run_CLM.m
@@ -1,9 +1,20 @@
-% Parameters 
+addpath(folder_path());
 P = 283;
-% Inputs
-u1 = [0 0 1 0 0 0 1];
-u2 = [0 0 0 1 1 0 1];
-q = [1 1 0];
+m = 8;  % 128-bit AES
+d = 4;  
+r = randi([0 1], 7, d);
+B = generator_matrix(P, d);
+H = [eye(m) B'];
 
-%CLM_mul(u1, u2, q, 25)
-CLM_pow([1 0 0 0 0 0 0], 2, [0 0 0], 25)
+in = randi([0, (2^m-1)]);
+dec2hex(in)
+
+bit8_input = flip(dec2bin(in)=='1');
+bit8_input = [bit8_input zeros(1, m - length(bit8_input))];
+L = isomorphism(P);
+L_inv = inverse_over_F2(L);
+
+
+x_in = mulAdd_module(mod(bit8_input*L,2), randi([0 1], 1, d),P,d);
+x_out = CLM_Sbox(x_in,r,P);
+flip(mod(x_out*H'*L_inv,2))
